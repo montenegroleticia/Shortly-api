935abe99a2e78bd069adc3b37566eee008229de1
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-05-19 11:52:24 -0300

    fix bug: confirmPassword on schema

--- src/schemas/auth.schemas.js
+++ src/schemas/auth.schemas.js
@@ -4,6 +4,7 @@ export const singUpSchema = joi.object({
   name: joi.string().required(),
   email: joi.string().email().required(),
   password: joi.string().min(3).required(),
+  confirmPassword: joi.string().min(3).required()
 })
 
 export const signInSchema = joi.object({
