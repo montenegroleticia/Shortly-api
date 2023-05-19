85fff9cb416ff772391dabb89d9cbb9b6e5e5ae3
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-05-19 11:14:31 -0300

    getUser and GetRanking

---
+++ src/controllers/users.controllers.js
@@ -0,0 +1,44 @@
+import { db } from "../database/database.connection.js";
+
+export async function getUser(req, res) {
+  try {
+    const { userId } = res.locals.session;
+    const user = await db.query(
+      ` SELECT
+      users.id,
+      users.name,
+      SUM(urls.visitsCount) AS visitCount,
+      json_agg(json_build_object(
+        'id', urls.id,
+        'shortUrl', urls.shortUrl,
+        'url', urls.url,
+        'visitCount', urls.visitsCount
+      )) AS shortenedUrls
+    FROM users
+    LEFT JOIN urls ON users.id = urls.userId
+    WHERE users.id = $1
+    GROUP BY users.id, users.name;
+  `,
+      [userId]
+    );
+    res.status(200).send(user.rows[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+export async function getRanking(req, res) {
+  try {
+    const users = await db.query(
+      `SELECT users.id, users.name, COUNT(urls.id) AS linksCount, SUM(urls.visitsCount) AS visitCount
+      FROM users
+      LEFT JOIN urls ON users.id = urls.userId
+      GROUP BY users.id, users.name
+      ORDER BY visitCount DESC
+      LIMIT 10;`
+    );
+    res.status(200).send(users.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
